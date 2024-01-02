dd4005a420f1ca18adb01c00e5477fb83170b180
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2024-01-02 22:21:21 +0200

    Project INIT

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+         $users = User::factory(10)->create();
+         $posts = Post::factory(200)
+             ->recycle($users)
+             ->create();
+         $comments = Comment::factory(200)
+             ->recycle($users)
+             ->recycle($posts)
+             ->create();
+         $testUser = User::factory()
+             ->has(Post::factory(45))
+             ->has(Comment::factory(100)->recycle($posts))
+             ->create([
+             'name' => 'Test User',
+             'email' => 'test@example.com',
+         ]);
+    }
+}
---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Container from "@/Components/Container.vue";
+
+defineProps({
+    posts: Object,
+})
+</script>
+
+<template>
+    <AppLayout>
+        <Container>
+            <template #header>Posts</template>
+            <ul class="divide-y">
+                <li v-for="post in posts" :key="post.id" class="px-2 py-4">
+                    <span class="font-semibold">
+                        {{post.title}}
+                    </span>
+                </li>
+            </ul>
+        </Container>
+    </AppLayout>
+</template>
