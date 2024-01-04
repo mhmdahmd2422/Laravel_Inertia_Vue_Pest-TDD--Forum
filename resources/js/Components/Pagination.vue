4040ede5e4ef7b4a5f66afce4dea3d2bb88e4594
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2024-01-05 00:11:57 +0200

    Show a post's comments in frontend

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -4,14 +4,26 @@ import {Link} from "@inertiajs/vue3";
 defineProps({
     links: Object,
     meta: Object,
+    only: {
+        type: Array,
+        default: () => [],
+    }
 })
 </script>
 
 <template>
     <div class="flex items-center justify-between border-t border-gray-200 mt-6 px-4 py-3 sm:px-6">
         <div class="flex flex-1 justify-between sm:hidden">
-            <Link :href="links.prev" preserve-scroll class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</Link>
-            <Link :href="links.next" preserve-scroll class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</Link>
+            <Link :href="links.prev"
+                  :only="only"
+                  preserve-scroll
+                  class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            >Previous</Link>
+            <Link :href="links.next"
+                  :only="only"
+                  preserve-scroll
+                  class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+            >Next</Link>
         </div>
         <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
             <div class="bg-white border border-gray-200 rounded-lg p-2">
@@ -27,20 +39,30 @@ defineProps({
             </div>
             <div>
                 <nav class="isolate inline-flex -space-x-px rounded-md bg-white shadow-sm" aria-label="Pagination">
-                    <Link :href="links.prev" preserve-scroll class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+                    <Link :href="links.prev"
+                          :only="only"
+                          preserve-scroll
+                          class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    >
                         <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
                         </svg>
                         <span>Previous</span>
                     </Link>
-                    <Link preserve-scroll v-for="link in meta.links.slice(1, -1)"
-                       :href="link.url"
-                       :class="{'relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0': !link.active,
-                        'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': link.active}"
+                    <Link v-for="link in meta.links.slice(1, -1)"
+                          :href="link.url"
+                          :only="only"
+                          preserve-scroll
+                          :class="{'relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0': !link.active,
+                          'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': link.active}"
                     >
                         {{link.label}}
                     </Link>
-                    <Link :href="links.next" preserve-scroll class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
+                    <Link :href="links.next"
+                          :only="only"
+                          preserve-scroll
+                          class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                    >
                         <span>Next</span>
                         <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                             <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
