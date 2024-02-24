f36fac30c289eabd7f15469c9483315d7341d501
Mohamed Ahmed <mhmdahmd2422@gmail.com>
2024-02-25 01:02:28 +0200

    refactor PostFactory.php to use markdown posts fixtures

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,14 +3,18 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Symfony\Component\Finder\SplFileInfo;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
+    private static Collection $fixtures;
+
     /**
      * Define the model's default state.
      *
@@ -23,5 +27,24 @@ class PostFactory extends Factory
             'title' => str(fake()->sentence)->beforeLast('.')->title(),
             'body' => Collection::times(4, fn() => fake()->realText(1250))->join(PHP_EOL.PHP_EOL),
         ];
+    }
+
+
+    public function withFixture(): static
+    {
+        $posts = static::getFixtures()
+            ->map(fn (string $contents) => str($contents)->explode("\n", 2))
+            ->map(fn (Collection $parts) => [
+                'title' => str($parts[0])->trim()->after('# '),
+                'body' => str($parts[1])->trim(),
+            ]);
+
+        return $this->sequence(...$posts);
+    }
+
+    private function getFixtures(): \Illuminate\Support\Collection
+    {
+        return self::$fixtures ??= collect(File::files(database_path('factories/fixtures/posts')))
+            ->map(fn (SplFileInfo $fileInfo) => $fileInfo->getContents());
     }
 }
